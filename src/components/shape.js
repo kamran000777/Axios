6d3a00b23dc023a4a9fe07503f001eac56d5cdec
Kamran Ansari <96384339+kamran000777@users.noreply.github.com>
2022-05-11 21:21:40 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
-// declare class
+module.exports=Circle;
+class Shape{
+    constructor(name){
+       this.name;
 
-// export class using module.exports
+    }
+
+    drawShape(){
+        return`${this.name} is drawn`;
+    }
+
+    calculateArea(length,width){
+        return 2*(length*width);
+        
+    }
+    color(color){
+        return `${color} is Applied on ${this.name}`;
+    }
+    
+}
+module.exports = Shape;
